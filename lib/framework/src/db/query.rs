fad5ddacddcdb386f4c1672176f79c0e45b4805f
neo <1100909+neowu@users.noreply.github.com>
2026-04-30 19:36:27 -0400

    add unit test

--- lib/framework/src/db/query.rs
+++ lib/framework/src/db/query.rs
@@ -89,3 +89,93 @@ pub(crate) fn build_conditions<'a, T>(
         cond.build_query(sql, &mut param_index, params);
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use std::marker::PhantomData;
+
+    use super::*;
+
+    struct Entity;
+
+    #[test]
+    fn test_build_update_single() {
+        let updates = vec![Update::new("col1", &42 as &QueryParam)];
+        let mut sql = String::from("UPDATE t");
+        let mut params: Vec<&QueryParam> = vec![];
+        let mut index = 1;
+        build_update::<Entity>(updates, &mut sql, &mut params, &mut index);
+        assert_eq!(sql, "UPDATE t SET col1 = $1");
+        assert_eq!(index, 2);
+        assert_eq!(params.len(), 1);
+    }
+
+    #[test]
+    fn test_build_update_multiple() {
+        let updates = vec![Update::new("col1", &1), Update::new("col2", &&"value")];
+        let mut sql = String::from("UPDATE t");
+        let mut params: Vec<&QueryParam> = vec![];
+        let mut index = 1;
+        build_update::<Entity>(updates, &mut sql, &mut params, &mut index);
+        assert_eq!(sql, "UPDATE t SET col1 = $1, col2 = $2");
+        assert_eq!(index, 3);
+        assert_eq!(params.len(), 2);
+    }
+
+    #[test]
+    fn test_build_conditions_empty() {
+        let mut sql = String::from("SELECT 1");
+        let mut params: Vec<&QueryParam> = vec![];
+        build_conditions::<Entity>(vec![], &mut sql, &mut params, 1);
+        assert_eq!(sql, "SELECT 1");
+        assert!(params.is_empty());
+    }
+
+    #[test]
+    fn test_build_conditions_in() {
+        let mut sql = String::from("SELECT 1");
+        let mut params: Vec<&QueryParam> = vec![];
+        build_conditions::<Entity>(
+            vec![Cond::In { column: "id", values: vec![&1 as &QueryParam, &2, &3], _entity: PhantomData }],
+            &mut sql,
+            &mut params,
+            1,
+        );
+        assert_eq!(sql, "SELECT 1 WHERE id IN ($1, $2, $3)");
+        assert_eq!(params.len(), 3);
+    }
+
+    #[test]
+    fn test_build_conditions() {
+        let mut sql = String::from("SELECT 1");
+        let mut params: Vec<&QueryParam> = vec![];
+        build_conditions::<Entity>(
+            vec![
+                Cond::Eq { column: "id", value: &10, _entity: PhantomData },
+                Cond::Eq { column: "name", value: &"name", _entity: PhantomData },
+                Cond::NotNull { column: "deleted_at", _entity: PhantomData },
+            ],
+            &mut sql,
+            &mut params,
+            1,
+        );
+        assert_eq!(sql, "SELECT 1 WHERE id = $1 AND name = $2 AND deleted_at IS NOT NULL");
+        assert_eq!(params.len(), 2);
+    }
+
+    #[test]
+    fn test_build_update_and_conditions() {
+        let mut sql = String::from("UPDATE t");
+        let mut params: Vec<&QueryParam> = vec![];
+        let mut index = 1;
+        build_update::<Entity>(vec![Update::new("col1", &99)], &mut sql, &mut params, &mut index);
+        build_conditions::<Entity>(
+            vec![Cond::Eq { column: "id", value: &10, _entity: PhantomData }],
+            &mut sql,
+            &mut params,
+            index,
+        );
+        assert_eq!(sql, "UPDATE t SET col1 = $1 WHERE id = $2");
+        assert_eq!(params.len(), 2);
+    }
+}
